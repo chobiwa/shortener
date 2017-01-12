72023e9a28b2de237d15e6b814604fb9ba825b1b
MLN <mlnkrish86@gmail.com>
2017-01-12 14:45:22 +0530

    removing dep

--- app/controllers/shortener/shortened_urls_controller.rb
+++ app/controllers/shortener/shortened_urls_controller.rb
@@ -1,11 +1,9 @@
-require 'voight_kampff'
-
 class Shortener::ShortenedUrlsController < ActionController::Base
   include Shortener
 
   def show
     token = ::Shortener::ShortenedUrl.extract_token(params[:id])
-    track = Shortener.ignore_robots.blank? || request.human?
+    track = Shortener.ignore_robots.blank?
     url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: params, track: track)
     redirect_to url[:url], status: :moved_permanently
   end
